59b82538ca9b1bf7e62661964019de2882e2ab5c
DylanITM <dylanarias1129116@correo.itm.edu.co>
2026-04-01 18:11:24 -0500

    Agrega Proyecto Blazor Web App y cambia Program.cs

--- AutoFleet.API/Program.cs
+++ AutoFleet.API/Program.cs
@@ -5,6 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
@@ -12,6 +14,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
     app.UseSwaggerUI(options =>
     {
         options.SwaggerEndpoint("/openapi/v1.json", "v1");
@@ -19,9 +22,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
