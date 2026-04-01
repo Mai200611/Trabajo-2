d46e125de8af70b01bd8e42671f709c87950b0c4
DylanITM <dylanarias1129116@correo.itm.edu.co>
2026-04-01 18:58:06 -0500

    Crea entidad Vehiculo y sus atributos

---
+++ AutoFleet.Shared/Entities/Vehiculo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoFleet.Shared.Entities
+{
+    internal class Vehiculo
+    {
+        public int Id { get; set; }
+
+        public string Placa { get; set; }
+
+        public string TipoVehiculo { get; set; }
+
+        public string Marca { get; set; }
+
+        public DateTime Año { get; set; }
+
+        public float CapacidadCargaKg { get; set; }
+
+        public string EstadoOperativo { get; set; }
+
+        public float KilometrajeActual { get; set; }
+    }
+}
