f67d856f3bb222a51b9c59c79f4136bad95131b8
CursedDaniel <cetroloco.daniel@gmail.com>
2026-04-02 16:24:17 -0500

    Se Completaron las Validaciones para los atributos de la entidad Manteniemiento

--- AutoFleet.Shared/Entities/Mantenimiento.cs
+++ AutoFleet.Shared/Entities/Mantenimiento.cs
@@ -21,13 +21,30 @@ namespace AutoFleet.Shared.Entities
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         public DateTime Fecha { get; set; }
 
-        [Display(Name = "Tipo de Mantenimiento")]
+        [Display(Name = "Tipo de Mantenimiento")] //Tipo de mantenimiento realizado (preventivo, correctivo, etc.)
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         [MaxLength(70)(, ErrorMessage = "El campo {0} no puede exceder los 70 caracteres.")]
         public string Tipo_Mantenimiento { get; set; }
+
+        [Display(Name = "Taller")] //NIT del taller donde se realizó el mantenimiento
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [StringLength(12, ErrorMessage = "El NIT del taller no puede exceder los 12 caracteres.")]
         public string Taller { get; set; }
+
+        [Display(Name = "Costo (COP)")] //Costo total del mantenimiento en pesos (COP)
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Costo { get; set; }
+
+        [Display(Name = "Detalles")] //Detalles adicionales del mantenimiento
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [MaxLength(200)(, ErrorMessage = "El campo {0} no puede exceder los 200 caracteres.")]
         public string Detalles { get; set; }
+
+        [DataType(DataType.Date)] //Fecha siguiente Mantenimiento
+        [Column(TypeName = "date")]
+        [Display(Name = "Siguiente Mantenimiento")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         public DateTime Proximo_Mantenimiento { get; set; }
 
     }
