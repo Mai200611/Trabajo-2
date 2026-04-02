73aec13e103d60c5b0a6c8553624d4ea23ac4e93
CursedDaniel <cetroloco.daniel@gmail.com>
2026-04-02 16:13:04 -0500

    Se Definen las Validaciones para la FK, la Fecha y el tipo de Mantenimiento

--- AutoFleet.Shared/Entities/Mantenimientos.cs
+++ AutoFleet.Shared/Entities/Mantenimientos.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using System.Text;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
 
 namespace AutoFleet.Shared.Entities
 {
     internal class Mantenimientos
     {
+        [Display(Name = "Vehiculo")]
+        [Required(ErrorMessage = "El campo Vehiculo es obligatorio.")]
         public int Id { get; set; }
+        [ForeignKey("Id_Vehiculo")] //FK de Vehiculo
+        public Vehiculo? Vehiculo { get; set; }
+
+        [DataType(DataType.Date)] //Fecha Mantenimiento
+        [Column(TypeName = "date")]
+        [Display(Name = "Fecha de Mantenimiento")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         public DateTime Fecha { get; set; }
+
+        [Display(Name = "Tipo de Mantenimiento")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [MaxLength(70)(, ErrorMessage = "El campo {0} no puede exceder los 70 caracteres.")]
         public string Tipo_Mantenimiento { get; set; }
         public string Taller { get; set; }
         public decimal Costo { get; set; }
