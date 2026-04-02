ce8d91e85db15ac8b0af0bd6959e875a41b12ea0
CursedDaniel <cetroloco.daniel@gmail.com>
2026-04-02 11:40:52 -0500

    Definir Entidad Mantenimientos, sin Validaciones

---
+++ AutoFleet.Shared/Entities/Mantenimientos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AutoFleet.Shared.Entities
+{
+    internal class Mantenimientos
+    {
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Tipo_Mantenimiento { get; set; }
+        public string Taller { get; set; }
+        public decimal Costo { get; set; }
+        public string Detalles { get; set; }
+        public DateTime Proximo_Mantenimiento { get; set; }
+
+    }
+}
