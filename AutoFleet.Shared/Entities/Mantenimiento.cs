c50e53008297deb6d0a3c7a5038f78439d521da9
DylanITM <dylanarias1129116@correo.itm.edu.co>
2026-04-01 19:32:48 -0500

    Agrega anotaciones de entity framework en Vehiculo.cs

--- AutoFleet.Shared/Entities/Mantenimiento.cs
+++ AutoFleet.Shared/Entities/Mantenimiento.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace AutoFleet.Shared.Entities
 {
-    internal class Recorrido
+    internal class Mantenimiento
     {
     }
 }
--- AutoFleet.Shared/Entities/Vehiculo.cs
+++ AutoFleet.Shared/Entities/Vehiculo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace AutoFleet.Shared.Entities
@@ -8,18 +9,43 @@ namespace AutoFleet.Shared.Entities
     {
         public int Id { get; set; }
 
+        [Display(Name = "Placa")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [MaxLength(10, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres.")]
         public string Placa { get; set; }
 
+
+        [Display(Name = "Tipo de Vehiculo")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [MaxLength(50, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres.")]
         public string TipoVehiculo { get; set; }
 
+        [Display(Name = "Marca")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [MaxLength(50, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres.")]
         public string Marca { get; set; }
 
-        public DateTime Año { get; set; }
+        [Display(Name = "Modelo")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [MaxLength(50, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres.")]
+        public string Modelo { get; set; }
 
+        [Display(Name = "Año")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd HH:mm}", ApplyFormatInEditMode = true)]
+        public int Año { get; set; }
+
+        [Display(Name = "Capacidad de Carga (kg)")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [Range(0, int.MaxValue, ErrorMessage = "El campo {0} debe estar entre {1} y {2} kg.")]
         public float CapacidadCargaKg { get; set; }
 
-        public string EstadoOperativo { get; set; }
+        [Display(Name = "Estado Operativo")]
+        [MaxLength(20, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres.")]
+        public string EstadoOperativo { get; set; } = "Disponible"; //default como 'Disponible'
 
+        [Display(Name = "Kilometraje Actual")]
+        [Range(0, 1000000, ErrorMessage = "El campo {0} debe estar entre {1} y {2} km.")] //Normalmente el contador/odometro llega hasta 1 millon
         public float KilometrajeActual { get; set; }
 
         public ICollection<Recorrido> Recorridos { get; set; }
