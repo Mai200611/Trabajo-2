aca88becc92eb2146dee9676ea08e91efef05598
DylanITM <dylanarias1129116@correo.itm.edu.co>
2026-04-03 15:43:50 -0500

    Agrega en DataContext los DbSet de todas las entidades

--- AutoFleet.API/Data/DataContext.cs
+++ AutoFleet.API/Data/DataContext.cs
@@ -9,7 +9,12 @@ namespace AutoFleet.API.Data
         {
         }
 
+        public DbSet<Vehiculo> Vehiculos { get; set; }
+        public DbSet<Conductor> Conductores { get; set; }
         public DbSet<Ruta> Rutas { get; set; }
+        public DbSet<Recorrido> Recorridos { get; set; }
+        public DbSet<Mantenimiento> Mantenimientos { get; set; }
+        public DbSet<Abastecimiento_Combustible> Abastecimientos_Combustible { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -17,6 +22,12 @@ namespace AutoFleet.API.Data
             {
                 entity.HasKey(r => r.CodRuta);
                 entity.Property(r => r.CodRuta).ValueGeneratedOnAdd();
+            });
+
+            modelBuilder.Entity<Recorrido>(entity =>
+            {
+                entity.HasKey(r => r.CodRecorrido);
+                entity.Property(r => r.CodRecorrido).ValueGeneratedOnAdd();
             });
 
             base.OnModelCreating(modelBuilder);
