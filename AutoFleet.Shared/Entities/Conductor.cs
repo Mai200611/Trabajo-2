30caed308b9cb70cbaab23e83b298fd62023935b
YeferxonMarquez <yeferxonalejandromarquezaraque@gmail.com>
2026-04-02 20:35:50 -0500

    Terminada Entidad Conductor

---
+++ AutoFleet.Shared/Entities/Conductor.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutoFleet.Shared.Entities
+{
+    public class Conductor
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [MaxLength(100)]
+        public string Nombre { get; set; } = null!;
+
+        [Required(ErrorMessage = "El documento es obligatorio")]
+        [MaxLength(20)]
+        public string Documento { get; set; } = null!;
+
+        [Required(ErrorMessage = "La licencia es obligatoria")]
+        [MaxLength(20)]
+        public string Licencia { get; set; } = null!;
+
+        [Required]
+        public string Categoria { get; set; } = null!;
+
+        [Required]
+        public DateTime FechaVencimiento { get; set; }
+
+        [MaxLength(15)]
+        public string? Telefono { get; set; }
+
+        public string? Direccion { get; set; }
+
+        public bool EstadoDisponibilidad { get; set; }
+
+        // Relación con Recorrido (Punto 4 de tu tarea)
+        // Nota: Si 'Recorrido' no existe aún, Visual Studio mostrará un error.
+        // Puedes comentarlo con // si necesitas que compile ya mismo.
+        public ICollection<Recorrido>? Recorridos { get; set; }
+    }
+}
