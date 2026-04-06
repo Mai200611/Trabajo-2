afbc2c8e0061fc21dad411fecd214d1cb27f863c
DylanITM <dylanarias1129116@correo.itm.edu.co>
2026-04-06 17:19:46 -0500

    Cambios menores

---
+++ AutoFleet.API/Controllers/ConductorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AutoFleet.API.Controllers
+{
+    public class ConductorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
