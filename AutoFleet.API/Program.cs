d97695008ce6b76ff4566e860150d754f64e6930
YeferxonMarquez <yeferxonalejandromarquezaraque@gmail.com>
2026-04-05 21:29:29 -0500

    Finalización de SCRUM-63 y 64: Configuración de DbContext y creación de migración inicial.

--- AutoFleet.API/Program.cs
+++ AutoFleet.API/Program.cs
@@ -1,12 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using AutoFleet.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+// ESTA ES LA LÍNEA QUE CONECTA TU DATACONTEXT
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConnection"));
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
-
 
 var app = builder.Build();
 
@@ -24,5 +29,4 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
-
 app.Run();
